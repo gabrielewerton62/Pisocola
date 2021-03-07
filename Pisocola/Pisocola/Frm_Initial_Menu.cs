966438596f29aa1fefdcb6827c0e9713b228a75c
Gabriel Rangel <gabriel.rangel@effetive.com.br>
2021-03-07 19:32:12 -0300

    Estrutura base do Backend

---
+++ Pisocola/Pisocola/Frm_Initial_Menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Pisocola.model;
+using Pisocola.dao;
+using System.Text.RegularExpressions;
+
+namespace Pisocola
+{
+    public partial class Frm_Initial_Menu : Form
+    {
+        public Frm_Initial_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //List<Object> list = CustomerDAO.GetInstance().GetCustomers();
+            
+            //Customer customer = (Customer)list[0];
+
+            //Customer customerGet = CustomerDAO.GetInstance().GetCustomer(1);
+
+            Customer customerIns = new Customer();
+
+            customerIns.SetIdCustomer(10);
+            customerIns.SetNmCustomer("NOME DO CLIENTE");
+            customerIns.SetNmSocial("ARGAMASA PERNAMBUCO");
+            customerIns.SetNrCpfCnpj("555.555.555-78");
+            customerIns.SetNrInsc("9999999-09");
+            customerIns.SetDsAddress("AV. COSTA AZUL, 320");
+            customerIns.SetNrPhone("98534-4236");
+
+            Customer updated = CustomerDAO.GetInstance().UpdateCustomer(customerIns);
+
+            Customer inserted = CustomerDAO.GetInstance().InsertCustomer(customerIns);
+
+            //CustomerDAO.GetInstance().DeleteCustomer(customerIns);
+
+            //Console.WriteLine("NOME DO CLIENTE: " + updated.GetNmCustomer());
+        }
+    }
+}
---
+++ Pisocola/Pisocola/dao/BaseDAO.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Pisocola.control;
+
+namespace Pisocola.dao
+{
+    public class BaseDAO
+    {
+        protected string tableName;
+
+        protected string columnParam;
+
+        protected List<Object> GetList(string sql)
+        {
+            MySqlConnection c = MySqlConnectionConfig.GetConnection();
+            c.Open();
+
+            MySqlCommand cmd = new MySqlCommand(sql, c);
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            List<Object> list = new List<object>();
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+               
+                while (dr.Read())
+                {
+                    Object obj = ProcessRow(dr);
+                    list.Add(obj);
+                }
+
+                dr.Close();
+                c.Close();
+                Console.WriteLine("Done.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return list;
+        }
+
+        protected Object GetItem(string sql)
+        {
+            List<Object> list = GetList(sql);
+
+            if (list != null && list.Count > 0)
+            {
+                return list[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        protected int instertItem(string sql, Object[] objs)
+        {
+
+            int lastTableId = 0;
+
+            try
+            {
+                MySqlConnection c = MySqlConnectionConfig.GetConnection();
+                c.Open();
+
+                Console.WriteLine("Connecting to MySQL...");
+
+                sql = replaceValues(sql, objs);
+
+                Console.WriteLine("SQL: " + sql);
+
+                MySqlCommand cmd = new MySqlCommand(sql, c);
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                dr.Close();
+
+                cmd = new MySqlCommand("SELECT auto_increment - 1 LAST_ID FROM information_schema.tables WHERE table_name = 'customer' AND table_schema = 'pisocola'", c);
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    lastTableId = Convert.ToInt32(dr["LAST_ID"]);
+                }
+
+                dr.Close();
+                c.Close();
+
+                Console.WriteLine("Done.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return lastTableId;
+        }
+
+        protected Object UpdateItem(string sql, Object[] paramObj, int idParam)
+        {
+
+            Object obj = null;
+
+            try
+            {
+                MySqlConnection c = MySqlConnectionConfig.GetConnection();
+                c.Open();
+
+                Console.WriteLine("Connecting to MySQL...");
+
+                sql = replaceValues(sql, paramObj);
+
+                Console.WriteLine("SQL: " + sql);
+
+                MySqlCommand cmd = new MySqlCommand(sql, c);
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                dr.Close();
+                c.Close();
+
+                Console.WriteLine("Done.");
+
+                obj = GetItem("SELECT * FROM " + tableName + " WHERE 1=1 AND " + columnParam + " = " + idParam);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return obj;
+        }
+
+        protected void DeleteItem(string sql, Object[] paramObj)
+        {
+            try
+            {
+                MySqlConnection c = MySqlConnectionConfig.GetConnection();
+                c.Open();
+
+                Console.WriteLine("Connecting to MySQL...");
+
+                sql = replaceValues(sql, paramObj);
+
+                Console.WriteLine("SQL: " + sql);
+
+                MySqlCommand cmd = new MySqlCommand(sql, c);
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                dr.Close();
+                c.Close();
+
+                Console.WriteLine("Done.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private string replaceValues(string sql, Object[] objs)
+        {
+
+            Object value;
+            string replace = "@";
+            int strIndex;
+
+            MatchCollection matches;
+
+            foreach (Object obj in objs)
+            {
+                matches = Regex.Matches(sql, replace);
+                value = obj;
+
+                foreach (Match match in matches)
+                {
+                    strIndex = match.Index;
+
+                    if (obj.GetType() == typeof(string))
+                    {
+                        value = (string)"'" + obj + "'";
+                    }
+                    else
+                    {
+                        value = Convert.ToInt32(value).ToString();
+                    }
+
+                    sql = sql.Remove(strIndex, 1).Insert(strIndex, (string)value);
+
+                    break;
+
+                }
+
+            }
+
+            return sql;
+        }
+
+        protected virtual Object ProcessRow(MySqlDataReader data)
+        {
+            Console.WriteLine("Nao Implementado");
+            return null;
+        }
+
+    }
+}
