ed6b8201cd5755e152600a890be74d3e5164f92b
Gabriel Rangel <gabriel.rangel@effetive.com.br>
2021-03-09 02:05:04 -0300

    Label da tela de Consulta de Clientes

--- Pisocola/Pisocola/Frm_New_Customer.Designer.cs
+++ Pisocola/Pisocola/Frm_New_Customer.Designer.cs
@@ -46,11 +46,12 @@
             this.Inpt_Cnpj = new System.Windows.Forms.MaskedTextBox();
             this.Inpt_Cpf = new System.Windows.Forms.MaskedTextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Inpt_Customer_Name
             // 
-            this.Inpt_Customer_Name.Location = new System.Drawing.Point(12, 44);
+            this.Inpt_Customer_Name.Location = new System.Drawing.Point(12, 72);
             this.Inpt_Customer_Name.Name = "Inpt_Customer_Name";
             this.Inpt_Customer_Name.Size = new System.Drawing.Size(230, 20);
             this.Inpt_Customer_Name.TabIndex = 5;
@@ -62,7 +63,7 @@
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new System.Drawing.Point(12, 48);
             this.label1.Name = "label1";
             this.label1.Padding = new System.Windows.Forms.Padding(2);
             this.label1.Size = new System.Drawing.Size(107, 21);
@@ -75,7 +76,7 @@
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(248, 20);
+            this.label2.Location = new System.Drawing.Point(248, 48);
             this.label2.Name = "label2";
             this.label2.Padding = new System.Windows.Forms.Padding(2);
             this.label2.Size = new System.Drawing.Size(91, 21);
@@ -84,7 +85,7 @@
             // 
             // Inpt_Social_Name
             // 
-            this.Inpt_Social_Name.Location = new System.Drawing.Point(248, 44);
+            this.Inpt_Social_Name.Location = new System.Drawing.Point(248, 72);
             this.Inpt_Social_Name.Name = "Inpt_Social_Name";
             this.Inpt_Social_Name.Size = new System.Drawing.Size(230, 20);
             this.Inpt_Social_Name.TabIndex = 8;
@@ -96,7 +97,7 @@
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(484, 20);
+            this.label3.Location = new System.Drawing.Point(484, 48);
             this.label3.Name = "label3";
             this.label3.Padding = new System.Windows.Forms.Padding(2);
             this.label3.Size = new System.Drawing.Size(47, 21);
@@ -132,9 +133,9 @@
             this.Field_Nr_Phone,
             this.Field_Dt_Insert});
             this.Grid_Customer_Consult.HideSelection = false;
-            this.Grid_Customer_Consult.Location = new System.Drawing.Point(15, 86);
+            this.Grid_Customer_Consult.Location = new System.Drawing.Point(12, 98);
             this.Grid_Customer_Consult.Name = "Grid_Customer_Consult";
-            this.Grid_Customer_Consult.Size = new System.Drawing.Size(1297, 463);
+            this.Grid_Customer_Consult.Size = new System.Drawing.Size(1300, 451);
             this.Grid_Customer_Consult.TabIndex = 12;
             this.Grid_Customer_Consult.UseCompatibleStateImageBehavior = false;
             this.Grid_Customer_Consult.View = System.Windows.Forms.View.Details;
@@ -173,7 +174,7 @@
             // 
             // Inpt_Cnpj
             // 
-            this.Inpt_Cnpj.Location = new System.Drawing.Point(484, 44);
+            this.Inpt_Cnpj.Location = new System.Drawing.Point(484, 72);
             this.Inpt_Cnpj.Mask = "00,000,000/0000-00";
             this.Inpt_Cnpj.Name = "Inpt_Cnpj";
             this.Inpt_Cnpj.Size = new System.Drawing.Size(111, 20);
@@ -181,7 +182,7 @@
             // 
             // Inpt_Cpf
             // 
-            this.Inpt_Cpf.Location = new System.Drawing.Point(601, 44);
+            this.Inpt_Cpf.Location = new System.Drawing.Point(601, 72);
             this.Inpt_Cpf.Mask = "000,000,000-00";
             this.Inpt_Cpf.Name = "Inpt_Cpf";
             this.Inpt_Cpf.Size = new System.Drawing.Size(106, 20);
@@ -193,18 +194,31 @@
             this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(601, 20);
+            this.label4.Location = new System.Drawing.Point(601, 48);
             this.label4.Name = "label4";
             this.label4.Padding = new System.Windows.Forms.Padding(2);
             this.label4.Size = new System.Drawing.Size(38, 21);
             this.label4.TabIndex = 15;
             this.label4.Text = "CPF";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 31);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Consulta de Clientes";
+            // 
             // Frm_New_Customer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1324, 561);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.Inpt_Cpf);
             this.Controls.Add(this.Inpt_Cnpj);
@@ -242,5 +256,6 @@
         private System.Windows.Forms.MaskedTextBox Inpt_Cnpj;
         private System.Windows.Forms.MaskedTextBox Inpt_Cpf;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
